5a0332a44506843627096510ec6848cc9eccfda1
silumansupra <lukman@untangledmedia.com>
2026-06-14 03:42:00 +0700

    remove duplicate name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
